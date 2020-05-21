9f8be65390a2a3253652730d0cb45a0f7a479299
prince11sysop <prince11rajput@gmail.com>
2020-05-21 21:19:15 +0530

    Greet new user

--- server/server.js
+++ server/server.js
@@ -22,11 +22,33 @@ io.on('connection', (socket)=>{
 
     socket.on('createMessage', (message)=>{
         console.log('createMessage',message);
+
+        //socket emit from admin
+        socket.emit('newMessage',{
+            from: 'Admin',
+            text: 'Welcome to the chat app', 
+            createdAt: new Date().getTime()
+        });
+
+        //socket.broadcast.emit from admin
+        socket.broadcast.emit('newMessage',{
+            from:'Admin',
+            text: 'New user joined',
+            createdAt: new Date().getTime()
+        });
+
+
         io.emit('newMessage',{
             from:message.from,
             text:message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage',{
+        //     from:message.from,
+        //     text: message.text,
+        //     createdAt:new Date().getTime()
+        // })
+
     });
 
     socket.on('disconnect', ()=>{
